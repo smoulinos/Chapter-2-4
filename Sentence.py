576d7539c0f556412f3776f7f4ff649985a240f1
smoulinos <31967585+smoulinos@users.noreply.github.com>
2017-10-06 08:21:00 -0400

    Add files via upload

---
+++ Sentence.py
@@ -0,0 +1,13 @@
+#Allwork and no play makes Jack a dull bo
+W1 = "All"
+W2 = "Work"
+W3 = "and"
+W4 = "no"
+W5 = "play"
+W6 = "makes"
+W7 = "Jack"
+W8 = "a"
+W9 = "dull"
+W10 = "boy"
+
+print (W1, W2, W3, W4, W5, W6, W7, W8, W9, W10)
